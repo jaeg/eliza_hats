0b81a6542888e42e9b5e292db8c19f02ab67e43f
John Godsey <johngodsey@gmail.com>
2019-04-11 23:54:53 -0400

    Adding more eliza bits

--- env/scripts/eliza.js
+++ env/scripts/eliza.js
@@ -1,3 +1,218 @@
+var model = {
+    ".*hello .*": [
+        "How do you do. Please state your problem."
+        ],
+    "(.*)? computer(.*)?": [
+        "Do computers worry you?",
+        "What do you think about machines?",
+        "Why do you mention computers?",
+        "What do you think machines have to do with your problem?",
+        ],
+    "(.*)? name(.*)?": [
+        "I am not interested in names",
+        ],
+    "(.*)?sorry(.*)?": [
+        "Please don't apologize",
+        "Apologies are not necessary",
+        "What feelings do you have when you apologize",
+        ],
+    "(.*)?I remember (.*)?": [
+        "Do you often think of $2?",
+        "Does thinking of $2 bring anything else to mind?",
+        "What else do you remember?",
+        "Why do you recall $2 right now?",
+        "What in the present situation reminds you of $2?",
+        "What is the connection between me and $2?",
+        ],
+    "(.*)?do you remember (.*)?": [
+        "Did you think I would forget $2?",
+        "Why do you think I should recall $2 now?",
+        "What about $2?",
+        "You mentioned $2",
+        ],
+    "(.*)?I want (.*)?": [
+        "What would it mean if you got $2?",
+        "Why do you want $2?",
+        "Suppose you got $2 soon."
+        ],
+    "(.*)?if (.*)?": [
+        "Do you really think it's likely that $2?",
+        "Do you wish that $2?",
+        "What do you think about $2?",
+        "Really--if $2?"
+        ],
+    "(.*)?I dreamt (.*)?": [
+        "How do you feel about $2 in reality?",
+        ],
+    "(.*)? dream (.*)?": [
+        "What does this dream suggest to you?",
+        "Do you dream often?",
+        "What persons appear in your dreams?",
+        "Don't you believe that dream has to do with your problem?",
+        ],
+    "(.*)?my mother (.*)?": [
+        "Who else in your family $2?",
+        "Tell me more about your family",
+        ],
+    "(.*)?my father (.*)?": [
+        "Your father?",
+        "Does he influence you strongly?",
+        "What else comes to mind when you think of your father?",
+        ],
+    "(.*)?I am glad(.*)?": [
+        "How have I helped you to be $2?",
+        "What makes you happy just now?",
+        "Can you explain why you are suddenly $2?",
+        ],
+    "(.*)?I am sad(.*)?": [
+        "I am sorry to hear you are depressed",
+        "I'm sure it's not pleasant to be sad",
+        ],
+    "(.*)? are like (.*)?": [
+        "What resemblence do you see between $1 and $2?",
+        ],
+    "(.*)? is like (.*)?": [
+        "In what way is it that $1 is like $2?",
+        "What resemblence do you see?",
+        "Could there really be some connection?",
+        "How?",
+        ],
+    "(.*)? alike (.*)?": [
+        "In what way?",
+        "What similarities are there?",
+        ],
+    "(.*)? same (.*)?": [
+        "What other connections do you see?",
+        ],
+    "(.*)?no(.*)?": [
+        "Why not?",
+        "You are being a bit negative.",
+        "Are you saying 'No' just to be negative?"
+        ],
+    "(.*)?I was (.*)?": [
+        "Were you really?",
+        "Perhaps I already knew you were $2.",
+        "Why do you tell me you were $2 now?"
+        ],
+    "(.*)? was I (.*)?": [
+        "What if you were $2?",
+        "Do you think you were $2?",
+        "What would it mean if you were $2?",
+        ],
+    "(.*)?I am (.*)?": [
+        "In what way are you $2?",
+        "Do you want to be $2?",
+        ],
+    "(.*)?am I (.*)?": [
+        "Do you believe you are $2?",
+        "Would you want to be $2?",
+        "You wish I would tell you you are $2?",
+        "What would it mean if you were $2?",
+        ],
+    "(.*)? am (.*)?": [
+        "Why do you say 'AM?'",
+        "I don't understand that"
+        ],
+    "(.*)?are you (.*)?": [
+        "Why are you interested in whether I am $2 or not?",
+        "Would you prefer if I weren't $2?",
+        "Perhaps I am $2 in your fantasies",
+        ],
+    "(.*)?you are (.*)?": [
+        "What makes you think I am $2?",
+        ],
+    "(.*)?because (.*)?": [
+        "Is that the real reason?",
+        "What other reasons might there be?",
+        "Does that reason seem to explain anything else?",
+        ],
+    "(.*)?were you (.*)?": [
+        "Perhaps I was $2?",
+        "What do you think?",
+        "What if I had been $2?",
+        ],
+    "(.*)?I can't (.*)?": [
+        "Maybe you could $2 now",
+        "What if you could $2?",
+        ],
+    "(.*)?I feel (.*)?": [
+        "Do you often feel $2?"
+        ],
+    "(.*)?I felt (.*)?": [
+        "What other feelings do you have?"
+        ],
+    "(.*)?I (.*)? you (.*)?z": [
+        "Perhaps in your fantasy we $2 each other",
+        ],
+    "(.*)?why don't you (.*)?": [
+        "Should you $2 yourself?",
+        "Do you believe I don't $2?",
+        "Perhaps I will $2 in good time",
+        ],
+    "(.*)?yes(.*)?": [
+        "You seem quite positive",
+        "You are sure?",
+        "I understand",
+        ],
+    "(.*)? someone (.*)?": [
+        "Can you be more specific?",
+        ],
+    "(.*)? everyone (.*)?": [
+        "Surely not everyone",
+        "Can you think of anyone in particular?",
+        "Who, for example?",
+        "You are thinking of a special person",
+        ],
+    "(.*)? always (.*)?": [
+        "Can you think of a specific example?",
+        "When?",
+        "What incident are you thinking of?",
+        "Really--always?",
+        ],
+    "(.*)?what (.*)?": [
+        "Why do you ask?",
+        "Does that question interest you?",
+        "What is it you really want to know?",
+        "What do you think?",
+        "What comes to your mind when you ask that?",
+        ],
+    "(.*)?perhaps (.*)?": [
+        "You do not seem quite certain",
+        ],
+    "(.*)?are (.*)?": [
+        "Did you think they might not be $2?",
+        "Possibly they are $2",
+        ]
+}
+
+var default_model = [
+    "Very interesting",
+    "I am not sure I understand you fully",
+    "What does that suggest to you?",
+    "Please continue",
+    "Go on",
+    "Do you feel strongly about discussing such things?",
+    ]
+
+function getStructure(input){
+    for (var key in model) {
+        var re = RegExp(key)
+        if(re.test(input)){
+            return key
+        }
+    }
+    return ""
+}
+
+function getReply(input) {
+  var key = getStructure(input)
+  if (key !== "") {
+    return model[key][Math.floor(Math.random() * model[key].length)]
+  } else {
+    return default_model[Math.floor(Math.random() * default_model.length)]
+  }
+}
+
 function init() {
   console.log("Eliza online")
 }
@@ -8,7 +223,7 @@ function main() {
     console.log(key)
     var id = key.split(":")[1]
     var message = redis.Do("get",key).String()[0]
-    redis.Do("set","ChatMessageResponse:"+id, "Response: " + message)
+    redis.Do("set","ChatMessageResponse:"+id, "Response: " + getReply(message))
   } else {
     console.log("Nothing")
   }
